62cc19c142f31b39e6be6221a5c26b0a1f846255
Débora Babo <babodebora@gmail.com>
2021-08-09 00:04:53 +0100

    :beetle: client comments correction; :up: protfolio annimation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+var current = 0,
+    slides = document.getElementsByClassName('quote-clients');
+
+setInterval(function() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+  current = (current != slides.length - 1) ? current + 1 : 0;
+  slides[current].style.opacity = 1;
+}, 5000);
+
+$(".rslides_portfolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+  
+});
